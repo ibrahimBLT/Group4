908489f18f35728d03c9cf8670db74827fd00e51
Halit-OTURK <halithamza0920@gmail.com>
2022-12-17 20:21:16 +0300

    created warmblooded class

---
+++ src/WarmBlooded.java
@@ -0,0 +1,20 @@
+public class WarmBlooded {
+
+
+
+}
+/*
+2-WarmBlooded
+Attributes
+-inherit from Animal Class attributes
+-static boolean hasStableTemp=true;
+-String fat (poor,normal, obese)
+
+Methods
+-constructor(super keyword)
+-Getter and Setter
+-regulateTemp()
+-defenseFungi()
+-getEnergy()(from their foods)
+-metabolicRate()(it is stable)
+ */
