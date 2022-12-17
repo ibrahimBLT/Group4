69b98d1d42efd3b1c981e35e5d961ecaeca34d92
Halit-OTURK <halithamza0920@gmail.com>
2022-12-17 21:44:47 +0300

    finished

--- src/WarmBlooded.java
+++ src/WarmBlooded.java
@@ -10,7 +10,7 @@ public class WarmBlooded extends Animal{
 
     public WarmBlooded(String name, String animalClass, String bodyCovering, String color, char gender, int age, int numberOfLegs, String size, double weight, String speed, String habitatArea, boolean canFly, boolean canSwim, String fat) {
         super(name, "Warmblooded", animalClass, bodyCovering, color, gender, age, numberOfLegs, size, weight, speed, habitatArea, canFly, canSwim);
-        this.fat = fat;
+        setFat(fat);
     }
     public static boolean isIsEndothermic() {
         return isEndothermic;
