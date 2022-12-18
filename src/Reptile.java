cafddb1c7b8ab97b22f2c2031ed39142669f0bf6
Emine Çalışkan <caliskanemine95@gmail.com>
2022-12-18 23:27:22 +0200

    fixed 100th times...

--- src/Reptile.java
+++ src/Reptile.java
@@ -1,47 +1,71 @@
-public class Reptile extends Animal{
+public class Reptile {
+
+    public class Reptile extends Animal {
 
 
-    private String tailSize;
+        private String tailSize;
+
+        //coldBloodedclass's attributes
+        static boolean hasStableTemp= false;
+        private String fat;
+
+        //getter and setter
+        public String getTailSize() {
+            return tailSize;
+        }
+
+        public static boolean isHasStableTemp() {
+            return hasStableTemp;
+        }
+
+        public String getFat() {
+            return fat;
+        }
+
+        public void setTailSize(String tailSize) {
+            this.tailSize = tailSize;
+        }
 
 
-    //getter and setter
-    public String getTailSize() {
-        return tailSize;
-    }
+        public static void setHasStableTemp(boolean hasStableTemp) {
+            Reptile.hasStableTemp = hasStableTemp;
+        }
 
-    public void setTailSize(String tailSize) {
-        this.tailSize = tailSize;
-    }
+        public void setFat(String fat) {
+            this.fat = fat;
+        }
 
+        @Override
+        public void setAnimalClass(String animalClass) {
+            super.setAnimalClass(animalClass);
+            setTailSize(tailSize);
+            setHasStableTemp(hasStableTemp);
+            setFat(fat);
 
-
-
-    @Override
-    public void setAnimalClass(String animalClass) {
-        super.setAnimalClass(animalClass);
-    }
-
-
-   // public void setColdBloodedClass(){}
+        }
 
 
-    //constructor (super keyword)
-    public Reptile(String name, String species, String animalClass, String bodyCovering, String color, char gender, int age, int numberOfLegs, String size, double weight, String speed, String habitatArea, boolean canFly, boolean canSwim) {
-        super(name, species, animalClass, bodyCovering, color, gender, age, numberOfLegs, size, weight, speed, habitatArea, canFly, canSwim);
-    }
+
+        //constructor (super keyword)
+        public Reptile(String name, String species, String animalClass, String bodyCovering, String color, char gender, int age, int numberOfLegs, String size, double weight, String speed, String habitatArea, boolean canFly, boolean canSwim) {
+            super(name, species, animalClass, bodyCovering, color, gender, age, numberOfLegs, size, weight, speed, habitatArea, canFly, canSwim);
+            setTailSize(tailSize);
+            setHasStableTemp(hasStableTemp);
+            setFat(fat);
+        }
 
 
-    //run method
-    public void run(String fast, String slow){
-        System.out.println();
+        //run method
+        public void run(){
+            System.out.println(getName()+" is running fast");
 
-    }
+        }
 
 
-    //changeHisColor method
-    public boolean changeHisColor(){
-        return changeHisColor();
-    }
+        //changeHisColor method
+        public void changeHisColor(){
+            System.out.println(getName()+" is changing color");;
+        }
 
        /*
     7-Reptiles
@@ -56,5 +80,7 @@ Methods
 -run() (fast or slow)
 -changeHisColor()
      */
+
+    }
 
 }
