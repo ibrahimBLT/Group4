a1c1803d1183c996131ba40979a039396b9a8b9c
Abdullah Sevindi <abdullahsevindi55@gmail.com>
2022-12-19 22:16:17 +0300

    abdullah

---
+++ src/GoldFish.java
@@ -0,0 +1,5 @@
+public class GoldFish extends Fish{
+    public GoldFish(String name, String color, char gender, int age, String size, double weight, String speed) {
+        super(name, color, gender, age, size, weight, speed);
+    }
+}
