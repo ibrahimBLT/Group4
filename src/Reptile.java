05fe983e64e1df15efbbe3bc1ddec70b1ada98aa
Emine Çalışkan <caliskanemine95@gmail.com>
2022-12-19 19:49:05 +0200

    changed something again

--- src/Reptile.java
+++ src/Reptile.java
@@ -1,55 +1,23 @@
-public class Reptile extends Animal {
+public class Reptile extends ColdBlooded {
 
 
         private String tailSize;
 
-        //coldBloodedclass's attributes
-        static boolean hasStableTemp= false;
-        private String fat;
 
-        //getter and setter
         public String getTailSize() {
             return tailSize;
         }
 
-        public static boolean isHasStableTemp() {
-            return hasStableTemp;
-        }
-
-        public String getFat() {
-            return fat;
-        }
 
         public void setTailSize(String tailSize) {
             this.tailSize = tailSize;
         }
 
 
-        public static void setHasStableTemp(boolean hasStableTemp) {
-            Reptile.hasStableTemp = hasStableTemp;
-        }
 
-        public void setFat(String fat) {
-            this.fat = fat;
-        }
-
-        @Override
-        public void setAnimalClass(String animalClass) {
-            super.setAnimalClass(animalClass);
-            setTailSize(tailSize);
-            setHasStableTemp(hasStableTemp);
-            setFat(fat);
-
-        }
-
-
-
-        //constructor (super keyword)
         public Reptile(String name, String species, String animalClass, String bodyCovering, String color, char gender, int age, int numberOfLegs, String size, double weight, String speed, String habitatArea, boolean canFly, boolean canSwim) {
             super(name, species, animalClass, bodyCovering, color, gender, age, numberOfLegs, size, weight, speed, habitatArea, canFly, canSwim);
             setTailSize(tailSize);
-            setHasStableTemp(hasStableTemp);
-            setFat(fat);
         }
 
 
@@ -65,7 +33,14 @@ public class Reptile extends Animal {
             System.out.println(getName()+" is changing color");;
         }
 
-       /*
+    @Override
+    public String toString() {
+        return "Reptile{" +
+                "tailSize='" + tailSize + '\'' +
+                '}';
+    }
+
+        /*
     7-Reptiles
 
 Attributes
